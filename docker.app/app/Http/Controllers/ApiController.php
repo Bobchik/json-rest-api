ff571c1502dee7ed3ef4b84724ee0740421200a0
Bobchik <workspace1936@gmail.com>
2018-06-25 20:03:03 +0300

    gitignore and ApiController fix

--- docker.app/app/Http/Controllers/ApiController.php
+++ docker.app/app/Http/Controllers/ApiController.php
@@ -18,6 +18,7 @@ class ApiController extends Controller
         $user->name = $request->name;
         $user->password = bcrypt($request->password);
         $user->save();
+
         return response([
             'status' => 'success',
             'data' => $user
